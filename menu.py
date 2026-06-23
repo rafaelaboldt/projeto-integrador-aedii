7b5be01c67dc638ddfdde98d9ef0567ee2042e09
Rafaela Boldt <rrafaboldt@gmail.com>
2026-06-23 11:34:40 -0300

    Add imports no menu

--- menu.py
+++ menu.py
@@ -1,14 +1,12 @@
-from ocorrencias import (
-    cadastrar_ocorrencia,
-    listar_ocorrencias,
-    atender_proxima_ocorrencia,
-    atender_maior_prioridade,
-    buscar_ocorrencia_por_id,
-    buscar_ocorrencias_por_nome_ou_tipo,
-    ordenar_ocorrencias,
-    ver_historico_acoes,
-    desfazer_ultima_acao,
-)
+from cadastrar_ocorrencia import cadastrar_ocorrencia
+from listar_ocorrencias import listar_ocorrencias
+from atender_proxima_ocorrencia import atender_proxima_ocorrencia
+from atender_maior_prioridade import atender_maior_prioridade
+from buscar_ocorrencia_por_id import buscar_ocorrencia_por_id
+from buscar_ocorrencias_por_nome_ou_tipo import buscar_ocorrencias_por_nome_ou_tipo
+from ordenar_ocorrencias import ordenar_ocorrencias
+from ver_historico_acoes import ver_historico_acoes
+from desfazer_ultima_acao import desfazer_ultima_acao
 
 
 while True:
