42cbbced7341c1005630e0d430158987ced78ce0
GabrieleQL <gabrielelapischies@gmail.com>
2026-06-29 23:59:02 -0300

    Add estrutura de lista e fila

--- estrutura.py
+++ estrutura.py
@@ -3,3 +3,5 @@ from collections import deque
 ocorrencias = {} # dicionário que armazena o cadastro das ocorrências
 fila_ocorrencia = deque() # fila que armazena a ordem das ocorrências cadastradas
 historico_acoes = [] # lista de logs
+fila_ocorrencia_ordenada = []
+ocorrencias_ordenadas = [] # lista que armazena as ocorrecias ordenadas por id
