dff62e0941a8aa086b6db7c325fed3ce8bf124b6
Rafaela Boldt <rrafaboldt@gmail.com>
2026-06-30 10:57:02 -0300

    Alterações no meunu

--- menu.py
+++ menu.py
@@ -14,12 +14,9 @@ while True:
     print("1 - Cadastrar ocorrência")
     print("2 - Listar todas as ocorrências")
     print("3 - Atender próxima ocorrência pela fila")
-    print("4 - Atender ocorrência de maior prioridade")
-    print("5 - Buscar ocorrência por ID")
-    print("6 - Buscar ocorrências por nome ou tipo")
-    print("7 - Ordenar ocorrências")
-    print("8 - Ver histórico de ações")
-    print("9 - Desfazer última ação")
+    print("4 - Buscar ocorrências por nome ou tipo")
+    print("5 - Ordenar ocorrências")
+    print("6 - Ver histórico de ações")
     print("0 - Sair")
 
     opcao = input("Escolha uma opção: ")
@@ -31,17 +28,11 @@ while True:
     elif opcao == "3":
         atender_proxima_ocorrencia()
     elif opcao == "4":
-        atender_maior_prioridade()
-    elif opcao == "5":
-        buscar_ocorrencia_por_id()
-    elif opcao == "6":
         buscar_ocorrencias_por_nome_ou_tipo()
-    elif opcao == "7":
+    elif opcao == "5":
         ordenar_ocorrencias()
-    elif opcao == "8":
+    elif opcao == "6":
         ver_historico_acoes()
-    elif opcao == "9":
-        desfazer_ultima_acao()
     elif opcao == "0":
         print("Saindo...")
         break
