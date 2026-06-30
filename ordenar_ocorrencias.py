e4e4468856c0409f1f357d8f8d826c4772e79cf1
GabrieleQL <gabrielelapischies@gmail.com>
2026-06-30 00:01:44 -0300

    Add function ordenar ocorrencias

--- ordenar_ocorrencias.py
+++ ordenar_ocorrencias.py
@@ -1,6 +1,24 @@
-from util import historico_acoes, adicionaLog, tempo, titulo
-from estrutura import deque, fila_ocorrencia, historico_acoes, ocorrencias
+from util import historico_acoes, buscarOcorrencia , adicionaLog, tempo, titulo, ordenarFilaOcorrencia
+from estrutura import deque, fila_ocorrencia, historico_acoes, ocorrencias, ocorrencias_ordenadas, fila_ocorrencia_ordenada
 
 def ordenar_ocorrencias():
-    """Listar todas as ocorrências cadastradas."""
-    print("Função listar_ocorrencias ainda não implementada.")
+    titulo("Ordernar Ocorrência por ID")
+
+    for i in fila_ocorrencia:
+        fila_ocorrencia_ordenada.append(i)
+
+    fila_ordenada = ordenarFilaOcorrencia(fila_ocorrencia_ordenada)
+
+    print(fila_ordenada) # ['BRU-504', 'ELI-282', 'LUI-510', 'MAR-613', 'SAB-704', 'TIT-416']
+
+    for i in fila_ordenada:
+        ocorrencia = buscarOcorrencia(i)
+        print(f"Ocorrencia buscada: {ocorrencia}")
+        if ocorrencia:
+            ocorrencias_ordenadas.append(ocorrencia)
+            print(f"Ocorrencia ordenada: {ocorrencias_ordenadas}")
+
+    print("Fila de Ocorrencias Ordenadas:")
+    print(ocorrencias_ordenadas)
+
+    
